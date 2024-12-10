3adb65bb37d15586cfc2b9d77388f81315bf0dce
Edwin Gonzales <edwin@newlogic.com>
2024-12-10 18:40:24 +0800

    Add spp_pos and spp_pos_id_redemption

---
+++ spp_pos_id_redemption/static/src/js/payment_screen.js
@@ -0,0 +1,29 @@
+/** @odoo-module */
+
+import {PaymentScreen} from "@point_of_sale/app/screens/payment_screen/payment_screen";
+import {patch} from "@web/core/utils/patch";
+
+patch(PaymentScreen.prototype, {
+    async _finalizeValidation() {
+        super._finalizeValidation();
+        for (const orderline of this.pos.get_order().orderlines) {
+            if (orderline.product.created_from_entitlement) {
+                const productId = orderline.product.id;
+                await this.orm.call("product.template", "redeem_voucher", [productId]);
+                orderline.product.voucher_redeemed = true;
+            }
+        }
+    },
+
+    backToPOS() {
+        const paymentLines = this.currentOrder.get_paymentlines();
+        this._removeCurrentPaymentLines(paymentLines);
+        this.pos.showScreen("ProductScreen");
+    },
+
+    async _removeCurrentPaymentLines(paymentLines) {
+        for await (const line of paymentLines) {
+            this.currentOrder.remove_paymentline(line);
+        }
+    },
+});
