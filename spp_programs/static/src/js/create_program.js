f094aa04d8e7ffd1825c48ad3bfc10ede5b44c0d
red <redick@newlogic.com>
2024-12-11 16:04:23 +0800

    [IMP] spp_programs: Improve importing order

--- spp_programs/static/src/js/create_program.js
+++ spp_programs/static/src/js/create_program.js
@@ -1,9 +1,8 @@
 /** @odoo-module **/
 
-import {patch} from "@web/core/utils/patch";
 import {ListController} from "@web/views/list/list_controller";
-
 import {onWillStart} from "@odoo/owl";
+import {patch} from "@web/core/utils/patch";
 
 patch(ListController.prototype, {
     setup() {
