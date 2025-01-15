de23e496c52c415c2877435b93a10308beab56a9
emjay0921 <michael@newlogic.com>
2025-01-15 12:18:21 +0800

    [FIX] spp_pos_id_redemption: remove fields on pos order

--- spp_pos_id_redemption/models/__init__.py
+++ spp_pos_id_redemption/models/__init__.py
@@ -5,5 +5,4 @@ from . import product_template
 from . import pos_session
 from . import res_partner
 from . import pos_config
-from . import pos_order
 from . import reg_id
--- spp_pos_id_redemption/models/pos_order.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class PosOrder(models.Model):
-    _inherit = "pos.order"
-
-    longitude = fields.Text()
-    latitude = fields.Text()
