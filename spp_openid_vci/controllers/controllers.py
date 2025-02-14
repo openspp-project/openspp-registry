5b8e738acc6e96f621dec8a57deca5d49b3b91d4
red <redick@newlogic.com>
2025-02-14 19:07:53 +0800

    [IMP] spp_openid_vci: improve api to verify jws

--- spp_openid_vci/controllers/controllers.py
+++ spp_openid_vci/controllers/controllers.py
@@ -1,6 +1,7 @@
 import json
 import logging
 
+import requests
 import werkzeug.wrappers
 
 from odoo.http import Controller, request, route
@@ -22,21 +23,76 @@ class SppOpenIDVCIController(Controller):
         return self.response_wrapper(code, error)
 
     def verify_jws(self, jws):
-        enc = request.env.ref("g2p_encryption.encryption_provider_default")
+        enc = request.env.ref("g2p_encryption.encryption_provider_default").sudo()
 
-        verified, jwt = enc.jwt_verify_jwcrypto(jws)
+        try:
+            verified, jwt = enc.jwt_verify_jwcrypto(jws)
+        except Exception as e:
+            _logger.error(e)
+            return False
 
         if not verified:
             return False
         return jwt
 
-    @route("/verify/vc", type="http", auth="none", methods=["Post"], csrf=False)
+    @classmethod
+    def get_individual_fields(cls):
+        return ["name"]
+
+    @route("/api/v1/registry/individual/<int:individual_id>", type="http", auth="none", methods=["GET"], csrf=False)
+    def get_individual_data(self, individual_id):
+        data = request.httprequest.data or "{}"
+        try:
+            data = json.loads(data)
+        except json.decoder.JSONDecodeError:
+            return self.response_wrapper(
+                400,
+                {
+                    "error": "Bad Request",
+                    "error_description": "data must be in JSON format.",
+                },
+            )
+        api_key = data.get("api_key")
+        if not api_key:
+            return self.error_wrapper(401, "Missing authentication credentials.")
+        user_id = request.env["res.users.apikeys"]._check_credentials(scope="rpc", key=api_key)
+        if not user_id:
+            return self.error_wrapper(401, "Invalid authentication credentials.")
+
+        individual = request.env["res.partner"].sudo().search([("id", "=", individual_id)], limit=1)
+        if not individual:
+            return self.error_wrapper(404, "Individual not found.")
+        return self.response_wrapper(
+            200, {"individual": individual.read(SppOpenIDVCIController.get_individual_fields())[0]}
+        )
+
+    @route("/verify/vc", type="http", auth="none", methods=["POST"], csrf=False)
     def verify_vc(self):
         data = request.httprequest.data or "{}"
-        data = json.loads(data)
+        try:
+            data = json.loads(data)
+        except json.decoder.JSONDecodeError:
+            return self.response_wrapper(
+                400,
+                {
+                    "error": "Bad Request",
+                    "error_description": "data must be in JSON format.",
+                },
+            )
+
+        # Check for authentication credentials
+        api_key = data.get("api_key")
+
+        if not api_key:
+            return self.error_wrapper(401, "Missing authentication credentials.")
+
+        # Verify credentials against res.users
+        user_id = request.env["res.users.apikeys"]._check_credentials(scope="rpc", key=api_key)
+
+        if not user_id:
+            return self.error_wrapper(401, "Invalid authentication credentials.")
 
         jws = data.get("jws")
-
         if not jws:
             return self.error_wrapper(400, "No JWS provided.")
 
@@ -44,4 +100,14 @@ class SppOpenIDVCIController(Controller):
         if jwt is False:
             return self.error_wrapper(401, "Invalid JWS provided.")
 
-        return self.response_wrapper(200, {"verified": True, "jwt": jwt.serialize()})
+        data = json.loads(jwt.claims)
+        individual_id_url = data.get("credential", {}).get("credentialSubject", {}).get("id")
+
+        response = requests.get(individual_id_url, json={"api_key": api_key})
+        if response.status_code != 200:
+            response_data = response.json()
+            error_code = response_data.get("error", {}).get("code", "Unknown error")
+            error_message = response_data.get("error", {}).get("message", "Unknown error")
+            return self.error_wrapper(error_code, error_message)
+
+        return self.response_wrapper(200, {"verified": True, **response.json()})
